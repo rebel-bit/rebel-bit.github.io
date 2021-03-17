52a6c477c5004d94f033f64e9359e5ddf95ca2aa
rebel-bit <67966711+rebel-bit@users.noreply.github.com>
2021-03-17 23:34:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,339 @@
+<?php
+//index.php
+?>
+<!DOTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta author="Faza Coders Family">
+    <meta title="Faza Coders Family">
+    <meta name="description" content="Faza Coders Family"/>
+    <meta name="keywords" content="Faza Coders Family"/>
+	<meta name="author" content="Faza Coders Family"/>
+	<meta name="description" content="Faza Coders Family"/>
+	<meta name="keywords" content="Faza Coders Family"/>
+	<meta name="format-detection" content="telephone=no"/>
+	<meta name="referrer" content="origin-when-cross-origin"/>
+	<meta property="fb:app_id" content="your_app_id" />
+	<meta name="google-signin-client-id" content="721724668570-nbkv1cfusk7kk4eni4pjvepaus73b13t.apps.googleusercontent.com">
+	<meta name="google-signin-scope"content="profile email https://www.fa7atolls.eu5.org/auth/developerprofiles http://fa7atolls.eu5.org">
+	<meta property="og:site_name" content="Fa7a Devloper">
+	<meta property="og:type" content="website">
+	<meta name="google-site-verification" content="+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34="/>
+	<meta name="robots" content="noindex,nofollow">
+	<meta http-equiv="refresh" content="fa7a tolls url=http://fa7a.eu5.org" />
+	<meta name="rating" content="adult" /> 
+	<meta property="og:image" content="https://c.top4top.io/p_1861fxqb53.jpeg">
+	<meta property="og:image:width" content="1200">
+	<meta property="og:image:height" content="630">
+	<meta property="og:locale" content="en">
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="rating" content="RTA-5042-1996-1400-1577-RTA" />
+	<meta property="og:url" content="http://www.blackearch.eu5.org" />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content="Coders Family Gans" />
+	<meta property="og:description" content="Selamat datang di website fa7a " />
+	<meta property="og:image" src="https://c.top4top.io/p_1861fxqb53.jpeg">
+	<meta name="twitter:creator" content="@F7tolls" />
+	<meta name="robots" content="noindex, nofollow">
+	<meta name="robots" content="index, follow">
+	<meta name="robots" content="noindex, follow">
+	<meta name="robots" content="index, nofollow">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="dns-prefetch" href="https://google.com">
+	<link rel="dns-prefetch" href="https://google.com">
+	<link rel="dns-prefetch" href="https://google.com">
+	<link rel="dns-prefetch" href="http://analytics.google.com">
+	<link rel="dns-prefetch" href="http://google.com">
+	<link rel="dns-prefetch" href="https://www.google.com">
+	<link rel="dns-prefetch" href="https://www.google.com">
+	<link rel="canonical" href="https://fa7atolls.eu5.org/">
+	<link rel="shortcut icon" href="https://c.top4top.io/p_1861fxqb53.jpeg" />
+	<!--- link css --->
+	<link rel="stylesheet" href="https://f7folio.epizy.com/wp_css/style.css">
+	<link rel="stylesheet" href="https://f7folio.epizy.com/wp_css/feather.css">
+	<link rel="stylesheet" href="https://f7folio.epizy.com/wp_css/slide.css">
+	<!---HTML shim end respond js for IE8 support of HTML --->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<title></title>
+	<style>
+	h4{
+	font-family: 'Iceland', cursive;
+	</style>
+	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+	<script>
+		$(document).ready(function(){
+			$(".siderbar_menu li").click(function(){
+			  $(".siderbar_menu li").removeClass("active");
+			  $(this).addClass("active");
+			});
+
+			$(".hamburger").click(function(){
+			  $(".wrapper").addClass("active");
+			});
+
+			$(".close, .bg_shadow").click(function(){
+			  $(".wrapper").removeClass("active");
+			});
+		});
+	</script>
+<!--Google Analytics-->
+<script>
+(fungsi (i, s, o, g, r, a, m) {i ['GoogleAnalyticsObject'] = r; i [r] = i [r] || function () {
+(i [r] .q = i [r] .q || []). push (argumen)}, i [r] .l = 1 * new Date (); a = s
+m = s.getElementsByTagName (o) [0]; a.async = 1; a.src = g; m.parentNode.insertBefore (a, m)
+}) (jendela, dokumen, 'script', 'https: //www.google-analytics.com/analytics.js','ga');
+
+ga ('create', 'UA-XXXXX-Y', 'auto');
+ga ('send', 'pageview');
+</script>
+<!-- Akhiri Google Analytics-->
+
+  <script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [{
+  "position": 1"
+  "name": "Pusat Google Penelusuran",
+  "item": "https://developers.google.com/search"
+   },{
+   "@type": "ListItem",
+   "position": 2,
+   "name": "Panduan Pemula untuk SEO",
+   "item": "https://developers.google.com/search/docs/beginner/get-started"
+   }]
+  }
+</script>
+<script type="e72b58bf81c97786f1f81aa7-text/javascript">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-PF1TQ4YFH6');
+</script>
+<noscript><iframe data-lazyloaded="1" src="about:blank" data-src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe><noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript></noscript>
+</head>
+<body>
+<div class="wrapper">
+  <div class="sidebar">
+    <div class="bg_shadow"></div>
+    <div class="sidebar_inner">
+        <div class="close">
+          <i class="feather-minimize"></i>
+        </div>
+        
+        <div class="profile_info">
+            <div class="profile_img">
+            </div>
+            <div class="profile_data">
+                <p class="name">faza id</p>
+                <span><i class="fas fa-map-marker-alt"></i> jateng, IND</span>
+            </div>
+        </div>
+      
+        <ul class="siderbar_menu">
+        </li>  
+        <li><a href="#">
+        <div class="icon"><i class="feather-code"></i></div>
+        <div class="title">Allat defacer</div>
+        <div class="arrow"><i class="feather-chevron-down"></i></div>
+        </a>
+        <ul class="accordion">
+        <li><a href="#">Account_Details_1</a></li>
+        <li><a href="#">Account_Details_2</a></li>
+        <li><a href="#">Account_Details_3</a></li>
+        <li><a href="#">Account_Details_4</a></li>
+        <li><a href="#">Account_Details_5</a></li>
+        </ul>
+        </li>  
+        </li>  
+        <li><a href="#">
+        <div class="icon"><i class="feather-code"></i></div>
+        <div class="title">Allat defacer</div>
+        <div class="arrow"><i class="feather-chevron-down"></i></div>
+        </a>
+        <ul class="accordion">
+        <li><a href="#">Account_Details_1</a></li>
+        <li><a href="#">Account_Details_2</a></li>
+        <li><a href="#">Account_Details_3</a></li>
+        <li><a href="#">Account_Details_4</a></li>
+        <li><a href="#">Account_Details_5</a></li>
+        </ul>
+        </li>  
+        <li><a href="#">
+        <div class="icon"><i class="feather-layers"></i></div>
+        <div class="title">Account Details</div>
+        <div class="arrow"><i class="feather-chevron-down"></i></div>
+        </li>
+        </a>
+        <ul class="accordion">
+        <li><a href="#">Account_Details_1</a></li>
+        <li><a href="#">Account_Details_2</a></li>
+        <li><a href="#">Account_Details_3</a></li>
+        <li><a href="#">Account_Details_4</a></li>
+        <li><a href="#">Account_Details_5</a></li>
+        </ul>
+        </li>  
+        <li><a href="#">
+        <div class="icon"><i class="feather-code"></i></div>
+          <div class="title">Account Details</div>
+               <div class="arrow"><i class="feather-chevron-down"></i></div>
+              </a>
+             <ul class="accordion">
+          <li><a href="#">Account_Details_1</a></li>
+          <li><a href="#">Account_Details_2</a></li>
+          <li><a href="#">Account_Details_3</a></li>
+          <li><a href="#">Account_Details_4</a></li>
+          <li><a href="#">Account_Details_5</a></li>
+        </ul>
+          </li>  
+          <li><a href="#">
+              <div class="icon"><i class="feather-github"></i></div>
+              <div class="title">Account Details</div>
+              <div class="arrow"><i class="feather-chevron-down"></i></div>
+              </a>
+            <ul class="accordion">
+                 <li><a href="#">Account_Details_1</a></li>
+                 <li><a href="#">Account_Details_2</a></li>
+                 <li><a href="#">Account_Details_3</a></li>
+                 <li><a href="#">Account_Details_4</a></li>
+                 <li><a href="#">Account_Details_5</a></li>
+              </ul>
+          </li>  
+          <li><a href="#">
+              <div class="icon"><i class="feather-gitlab"></i></div>
+              <div class="title">Previous Orders</div>
+              <div class="arrow"><i class="feather-chevron-down"></i></div>
+              </a>
+            <ul class="accordion">
+                 <li><a href="#">Previous_Orders_1</a></li>
+                 <li><a href="#">Previous_Orders_2</a></li>
+                 <li><a href="#">Previous_Orders_3</a></li>
+                 <li><a href="#">Previous_Orders_4</a></li>
+                 <li><a href="#">Previous_Orders_5</a></li>
+              </ul>
+          </li>  
+          <li><a href="#">
+              <div class="icon"><i class="feather-activity"></i></div>
+              <div class="title">Payment Info</div>
+              <div class="arrow"><i class="feather-chevron-down"></i></div>
+              </a>
+              <ul class="accordion">
+                 <li><a href="#">Payment_Info_1</a></li>
+                 <li><a href="#">Payment_Info_2</a></li>
+                 <li><a href="#">Payment_Info_3</a></li>
+                 <li><a href="#">Payment_Info_4</a></li>
+                 <li><a href="#">Payment_Info_5</a></li>
+              </ul>
+            
+          </li>  
+          <li><a href="#">
+              <div class="icon"><i class="feather-power"></i></div>
+              <div class="title">tester clikjaking</div>
+              </a></li>  
+          </li>  
+          <li><a href="https://f7folio.epizy.com/folio">
+          <div class="icon"><i class="feather-eye"></i></div>
+          <div class="title">halaman folio</div>
+          </a></li>  
+     </li>  
+     <li><a href="https://f7folio.epizy.com/crud">
+     <div class="icon"><i class="feather-eye"></i></div>
+     <div class="title">halaman crud</div>
+     </a></li>  
+        </ul>
+        </div>
+    </li>
+    <li><a href="https://rebel-bit.github.io">
+    <div class="icon"><i class="feather-github"></i></div>
+    <div class="title">halaman github.io</div>
+    </a></li>  
+    </ul>
+    </div>
+    
+      
+    </div>
+  </div>
+  <div class="main_container">
+    <div class="navbar">
+       <div class="hamburger">
+         <i class="fas fa-bars"></i>
+       </div>
+       <div class="logo">
+         <h4><a href="#">fazatolls</a></h4>
+      </div>
+    </div>
+    <div class="content">
+      <center>
+      <div class="item"><h3>@copyright 2021 muhamad faza akbar maualana bila ingin source hub saya +6289510104497</h3>
+      </center>
+      <div class="item"><?php
+      $filename = 'counter.txt';	//mendefinisikan nama file untuk menyimpan counter
+      
+      function counter(){		//function counter
+      global $filename;	//set global variabel $filename
+      
+      if(file_exists($filename)){		//jika file counter.txt ada
+      $value = file_get_contents($filename);	//set value = nilai di notepad
+      }else{		//jika file counter.txt tidak ada maka akan membuat file counter.txt
+      $value = 0;		//kemudian set value = 0
+      }
+      
+      file_put_contents($filename, ++$value);		//menuliskan kedalam file counter.txt value+1
+      }
+      
+      counter();	//menjalankan function counter
+      
+      echo '<center><h4>Total Dikunjungi: '.file_get_contents($filename);	//menampilkan counter di website . '<center></h4>'
+      ?></div>
+      <div class="item">SSL atau Secure Socket Layer adalah cara sebuah situs web membuat sambungan aman dengan browser web pengguna. Setiap kali seorang surfer web mengunjungi situs yang aman yang menggunakan teknologi SSL, menciptakan sebuah link yang terenkripsi antara sesi browser mereka dan web server. SSL adalah standar industri untuk komunikasi web yang aman dan digunakan untuk melindungi jutaan transaksi online setiap hari. </div>
+      <div class="item">Serangan Clickjacking adalah teknik jahat untuk menipu pengguna agar mengklik button yang berbahaya yang tidak di ketahui nya di web maupun link</div>
+      <div class="item">XSS merupakan kependekan yang digunakan untuk istilah cross site scripting XSS merupakan salah satu jenis serangan injeksi code (code injection attack)</div>
+      <div class="item">Bug SQL Injection berbahaya ? 1) Teknik ini memungkinkan seseorang dapat login kedalam sistem tanpa harus memiliki account. 2) Selain itu SQL injection juga memungkinkan seseorang merubah, menghapus, maupun menambahkan data–data yang berada didalam database..</div>
+      <div class="item">Bug Insecure Direct Object References (IDOR) ini merupakan bug yang sangat sederhana, sangat umum dan biasanya sangat berbahaya. Intinya pengguna aplikasi bisa mengubah input sedemikian hingga bisa mengakses objek/data yang bukan miliknya.</div>
+      <div class="item">fa7a tolls di buat  oleh faza 2021 dengan konten pemerograman,dari bug dan coding</div>
+      <div class="item"><td><?php
+      echo "Your User Agent is :" . $_SERVER ['HTTP_USER_AGENT'];
+      ?></td>
+      <div class="iten"><td><?php
+      echo "Tanggal dan Waktu sekarang adalah " . date("Y-m-d h:i:sa") . "<br>";
+      ?></td></div>
+      <div class="slideshow-container">
+      <div class="mySlides fade">
+      <div class="numbertext">1 / 3</div>
+      <img src="https://i.ibb.co/XxbZtGw/images-2.png" style="width:100%">
+      <div class="text">Pemrograman adalah proses menulis, menguji dan memperbaiki (debug), dan memelihara kode yang membangun suatu program komputer. ... Tujuan dari pemrograman adalah untuk memuat suatu program yang dapat melakukan suatu perhitungan atau 'pekerjaan' sesuai dengan keinginan si pemrogram.</div>
+      </div>
+      <div class="mySlides fade">
+      <div class="numbertext">2 / 3</div>
+      <img src="https://i.ibb.co/XxbZtGw/images-2.png" style="width:100%">
+      <div class="text">Program bug bounty adalah kesepakatan yang ditawarkan oleh banyak situs web, organisasi, dan pengembang perangkat lunak di mana individu dapat menerima pengakuan dan kompensasi [1] untuk melaporkan bug , terutama yang berkaitan dengan eksploitasi keamanan dan kerentanan .</div>
+      </div>
+      <div class="mySlides fade">
+      <div class="numbertext">3 / 3</div>
+      <img src="https://i.ibb.co/FX7XBSs/images-1.png" style="width:100%">
+      <div class="text">coding adalah hal yang berbeda dari pemerograman coding atau kegiatan menulis code sangatlah penting dalam memahami bahasa pemrograman. Terutama bagi mereka yang sama sekali belum pernah terjun ke dunia ini. Nyatanya, bisa menjadi sesuatu yang menarik dan menyenangkan. Apalagi saat ini ada cukup banyak alasan untuk memulai belajar coding. Mulai dari karir hingga membuka lapangan kerja sendiri bisa menjadi alasan untuk memulai belajar hal ini.</div>
+      </div>
+      </div>
+      <div style="text-align:center">
+      <span class="dot"></span> 
+      <span class="dot"></span> 
+      <span class="dot"></span> 
+      </div>
+      </td>
+      </td></div>
+    </div>
+  </div>
+</div>
+<script rel="preconnect" src="https://ajax.cloudflare.com"></script>
+<script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js"></script>	
+<script src="https://f7folio.epizy.com/wp_js/slide.js"></script>
+</body>
+</html>
